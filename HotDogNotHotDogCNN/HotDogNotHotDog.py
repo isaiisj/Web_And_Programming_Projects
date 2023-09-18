f0037117725eae35bb50947e81961c32b48206f0
Isai <47548422+isaiisj@users.noreply.github.com>
2023-09-18 00:20:55 -0600

    Update HotDogNotHotDog.py

--- HotDogNotHotDogCNN/HotDogNotHotDog.py
+++ HotDogNotHotDogCNN/HotDogNotHotDog.py
@@ -7,3 +7,27 @@ from tensorflow.keras import datasets,layers,models
 import tensorflow_datasets as tfds
 
 ds,ds_info = tfds.load('food101',shuffle_files = True,as_supervised=True, with_info = True)
+
+train_ds, valid_ds = ds["train"], ds["validation"]
+
+fig = tfds.show_examples(train_ds, ds_info)
+
+MAX_SIDE_LEN = 128
+HOT_DOG_CLASS = 55
+train_ds = train_ds.map(
+    lambda image, label: (tf.cast(tf.image.resize(image,[MAX_SIDE_LEN, MAX_SIDE_LEN]), dtype=tf.int32),
+                          tf.cast(label == HOT_DOG_CLASS, tf.int32))
+)
+valid_ds = valid_ds.map(
+    lambda image, label: (tf.cast(tf.image.resize(image,[MAX_SIDE_LEN, MAX_SIDE_LEN]), dtype=tf.int32),
+                          tf.cast(label == HOT_DOG_CLASS, tf.int32))
+)
+
+fig = tfds.show_examples(train_ds, ds_info)
+
+train_hd_size, valid_hd_size = 750, 250
+train_hotdogs = train_ds.filter(lambda image, label: label == 1).repeat(3)
+train_nothotdogs = train_ds.filter(lambda image, label: label == 0)
+
+valid_hotdogs = valid_ds.filter(lambda image, label: label == 1).repeat(3)
+valid_nothotdogs = valid_ds.filter(lambda image, label: label == 0)

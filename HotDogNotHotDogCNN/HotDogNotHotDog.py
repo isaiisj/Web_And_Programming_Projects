72fb92425b8e03b74f4a029f98c2c5e10fce9b81
Isai <47548422+isaiisj@users.noreply.github.com>
2023-09-18 00:55:43 -0600

    Update HotDogNotHotDog.py

--- HotDogNotHotDogCNN/HotDogNotHotDog.py
+++ HotDogNotHotDogCNN/HotDogNotHotDog.py
@@ -31,3 +31,28 @@ train_nothotdogs = train_ds.filter(lambda image, label: label == 0)
 
 valid_hotdogs = valid_ds.filter(lambda image, label: label == 1).repeat(3)
 valid_nothotdogs = valid_ds.filter(lambda image, label: label == 0)
+
+batch_size = 16
+train_ds = tf.data.Dataset.sample_from_datasets([train_hotdogs, train_nothotdogs],[0.5, 0.5], stop_on_empty_dataset=True)
+train_ds = train_ds.cache().batch(batch_size).prefetch(tf.data.AUTOTUNE)
+
+valid_ds = tf.data.Dataset.sample_from_datasets([valid_hotdogs, valid_nothotdogs],[0.5, 0.5], stop_on_empty_dataset=True)
+valid_ds = valid_ds.cache().batch(batch_size).prefetch(tf.data.AUTOTUNE)
+
+for image_batch, label_batch in train_ds.take(1):
+  print(image_batch)
+  print(label_batch)
+
+#Neural net implementation
+
+random.seed(0)
+model = models.Sequential()
+model.add(layers.Rescaling(1./255))
+model.add(layers.Conv2D(128,(3,3), activation='relu', input_shape=[MAX_SIDE_LEN,MAX_SIDE_LEN,3]))
+model.add(layers.MaxPooling2D(2,2))
+model.add(layers.Conv2D(64,(3,3),activation='relu'))
+model.add(layers.MaxPooling2D(2,2))
+model.add(layers.Conv2D(64,(3,3),activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dense(1))

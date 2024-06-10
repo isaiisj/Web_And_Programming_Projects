5769d9d7effb60ed42defb72307f588862a8d448
Isai <47548422+isaiisj@users.noreply.github.com>
2024-06-09 22:51:17 -0600

    Create SolveMeFirst.py

---
+++ HackerRank/Algorithms/SolveMeFirst.py
@@ -0,0 +1,37 @@
+''' Example
+a = 7
+b = 3
+
+Return 10
+
+Function Description
+
+Complete the solveMeFirst Function
+
+solveMeFirst has the following parameters:
+
+int a: the first value
+int b: the second value
+Returns
+- int: the sum of a and b
+
+
+Sample Input
+
+a = 2
+b = 3
+Sample Output
+
+5
+Explanation
+2 + 3 = 5 '''
+
+
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    return a+b
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)

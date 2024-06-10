636a0245dcc7aedd529084ed11ee9d4d0c3efced
Isai <47548422+isaiisj@users.noreply.github.com>
2024-06-09 22:59:34 -0600

    Create SimpleArraySum.py

---
+++ HackerRank/Algorithms/SimpleArraySum.py
@@ -0,0 +1,53 @@
+'''
+Given an array of integers, find the sum of its elements.
+
+For example, if the array ar=[1,2,3] , 1+2+3=6 , so return 6 .
+
+Function Description
+
+Complete the simpleArraySum function in the editor below. 
+It must return the sum of the array elements as an integer.
+
+simpleArraySum has the following parameter(s):
+
+ar: an array of integers
+
+Explanation
+
+We print the sum of the array's elements: 1 + 2 + 3 + 4 + 10 + 11 = 31 .
+'''
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    # Write your code here
+    sum = 0
+    for i in ar:
+        sum += i
+    return sum
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()

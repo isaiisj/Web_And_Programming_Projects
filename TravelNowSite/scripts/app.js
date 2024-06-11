ec2371a462b3e946a977f01774ae933db703f600
Isai <47548422+isaiisj@users.noreply.github.com>
2024-06-10 20:44:07 -0600

    Add files via upload

---
+++ TravelNowSite/scripts/app.js
@@ -0,0 +1,10 @@
+var app = (function(){
+    /* Properties */
+    var websiteName = "Travel-NOW";
+    /* Methods */
+    return{
+        getWebsiteName: function(){
+            return websiteName;
+        }
+    }
+}) ();

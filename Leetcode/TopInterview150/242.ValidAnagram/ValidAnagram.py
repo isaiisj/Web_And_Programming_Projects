e5a915229d131731fa01f746de7099e2567b066b
Isai <47548422+isaiisj@users.noreply.github.com>
2024-07-03 04:23:29 -0600

    Update ValidAnagram.py

--- Leetcode/TopInterview150/242.ValidAnagram/ValidAnagram.py
+++ Leetcode/TopInterview150/242.ValidAnagram/ValidAnagram.py
@@ -14,7 +14,12 @@ Example 2:
 
 Input: s = "rat", t = "car"
 Output: false
- 
+
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
 '''
 
 class Solution(object):

3ec999d06f29545e081f81dd38332b13580998af
Isai <47548422+isaiisj@users.noreply.github.com>
2024-07-03 04:24:03 -0600

    Update RansomNote.py

--- Leetcode/TopInterview150/383.RansomNote/RansomNote.py
+++ Leetcode/TopInterview150/383.RansomNote/RansomNote.py
@@ -17,6 +17,12 @@ Example 3:
 
 Input: ransomNote = "aa", magazine = "aab"
 Output: true
+
+
+Constraints:
+
+1 <= ransomNote.length, magazine.length <= 105
+ransomNote and magazine consist of lowercase English letters.
 '''
 
 class Solution(object):

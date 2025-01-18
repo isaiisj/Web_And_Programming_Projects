a7caa46c2bba3580c2f0b7bdce3284065714d4e7
Isai <47548422+isaiisj@users.noreply.github.com>
2025-01-18 01:44:35 -0600

    Create TwoSum.py

---
+++ Leetcode/Problems/0001.TwoSum/TwoSum.py
@@ -0,0 +1,54 @@
+'''
+Given an array of integers nums and an integer target, return 
+indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, 
+and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+'''
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        #create an empty dict
+        dict1 = {}
+
+        #iterate trough the nums list
+        for i,n in enumerate(nums):
+            complement = target - n   #obtain complement to target
+            if complement in dict1:   #if we have the compliment in dict
+                return [i, dict1[complement]]
+            else:
+                dict1[n] = i   #else add and go to the next number
+        
+#Time complexity: O(n)

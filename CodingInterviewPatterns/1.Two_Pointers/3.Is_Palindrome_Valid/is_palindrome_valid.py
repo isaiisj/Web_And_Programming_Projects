7deb8548026190f3c0ee286435756f5f477be776
Isai <47548422+isaiisj@users.noreply.github.com>
2025-07-16 00:31:54 -0600

    Create is_palindrome_valid.py

---
+++ CodingInterviewPatterns/1.Two_Pointers/3.Is_Palindrome_Valid/is_palindrome_valid.py
@@ -0,0 +1,49 @@
+'''
+
+A palindrome is a sequence of characters that reads the same forward and backward.
+
+Given a string, determine if it's a palindrome after removing all non-alphanumeric characters.
+A character is alphanumeric if it's either a letter or a number.
+
+Example 1:
+Input: s = 'a dog! a panic in a pagoda.'
+Output: True
+
+Example 2:
+Input: s = 'abc123'
+Output: False
+
+Constraints:
+The string may include a combination of lowercase English letters, numbers, spaces, and punctuations.
+
+'''
+
+
+def is_palindrome_valid(s: str) -> bool:   
+
+    # Create left and right pointer 
+    left: int = 0
+    right: int = len(s) - 1
+
+    # Iterate the array while left < right
+    while left < right:
+        # While left < right and the item is not alphanumeric
+        # keep increasing the left pointer
+        while left < right and not s[left].isalnum():
+            left += 1
+
+        # While left < right and the item is not alphanumeric
+        # keep decreasing the right pointer
+        while left < right and not s[right].isalnum():
+            right -= 1
+        
+        if s[left] == s[right]:
+            left += 1
+            right -= 1
+        else:
+            return False
+
+    return True
+
+
+# Time complexity: O(n)

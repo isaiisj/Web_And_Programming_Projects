96e03db8819d7df5b71de8690a1a1b4ab256b394
Isai <47548422+isaiisj@users.noreply.github.com>
2025-07-16 00:01:15 -0600

    Create TwoSum2InputArrayIsSorted.js

---
+++ Leetcode/TopInterview150/167.TwoSum2InputArrayIsSorted/TwoSum2InputArrayIsSorted.js
@@ -0,0 +1,81 @@
+/*
+
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order,
+find two numbers such that they add up to a specific target number. Let these two numbers 
+be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+Your solution must use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+Example 2:
+
+Input: numbers = [2,3,4], target = 6
+Output: [1,3]
+Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+Example 3:
+
+Input: numbers = [-1,0], target = -1
+Output: [1,2]
+Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+ 
+
+Constraints:
+
+2 <= numbers.length <= 3 * 104
+-1000 <= numbers[i] <= 1000
+numbers is sorted in non-decreasing order.
+-1000 <= target <= 1000
+The tests are generated such that there is exactly one solution.
+
+*/
+
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+
+    // Create left and right pointer
+    let left = 0;
+    let right = numbers.length - 1;
+
+    // Iterate the array while left < right
+    while (left < right) {
+
+        // Store the current sum 
+        let summ = numbers[left] + numbers[right];
+
+        // If the sum equals the target return the indexes
+        if (summ == target) {
+            return new Array(left +1, right + 1);
+        }
+
+        // If the sum is less increase left pointer
+        if (summ < target) {
+            left++;
+        }
+
+        // If the sum is greater decrease right pointer
+        if (summ > target) {
+            right --;
+        }
+    }
+
+    // If there are not index, return empty array
+    return new Array();
+};
+
+
+// Time complexity: O(n)

3bc1559f9422bdb35686c2d8d786e5da4a395978
Isai <47548422+isaiisj@users.noreply.github.com>
2025-08-28 11:05:49 -0600

    Create largest_container.py

---
+++ CodingInterviewPatterns/1.Two_Pointers/4.Largest_Container/largest_container.py
@@ -0,0 +1,47 @@
+'''
+
+You are given an array of numbers, each representing the height of a vertical line on a graph. 
+A container can be formed with any pair of these lines, along with the x-axis of the graph. 
+Return the amount of water which the largest container can hold.
+
+Input: heights = [2, 7, 8, 3, 7, 6]
+Output: 24
+
+'''
+
+
+from typing import List
+
+def largest_container(heights: List[int]) -> int:
+    
+    # Declare left and right pointer
+    left, right = 0, len(heights) - 1
+
+    # Max water
+    max_water = 0
+
+    # While left < right continue
+    while left < right:
+
+        # Get the current area (base * height)
+        area = min(heights[left],heights[right]) * (right - left)
+
+        # Update max current
+        if area > max_water :
+            max_water = area
+
+        # If left is less move left one
+        if heights[left] < heights[right]:
+            left += 1
+        
+        # If left is greater decrease right
+        elif heights[left] > heights[right]:
+            right -= 1
+        
+        # If both are equal move both
+        else:
+            left += 1
+            right -= 1
+        
+    # Return max area of water
+    return max_water
